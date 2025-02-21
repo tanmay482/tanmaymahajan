c3286fd3d341dc46fdde7ba44eea2fab4e2e5fdc
Azeem Shiakh <114724946+azshaikh0786@users.noreply.github.com>
2025-02-21 08:36:45 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
 
       {/* Navigation Links */}
       <nav className="my-4">
-        {["Home", "Skills","Education", "Certifications", "Projects"].map((item) => (
+        {["Skills","Education", "Certifications", "Projects"].map((item) => (
           <a
             key={item}
             href={`#${item.toLowerCase()}`}
